885de3546c12a8ef111f36d1f8043f42c14059bf
sunny-lee3 <sunnylee3712@gmail.com>
2021-04-14 20:29:32 -0700

    updated the functions of the sales page

--- js/app.js
+++ js/app.js
@@ -7,13 +7,15 @@ function Locations(name, photo, min, max, avgCustomer) {
     this.min = min;
     this.max = max;
     this.avgCustomer = avgCustomer;
-  }
+
+}
 function randCustNum(min, max)  {
-  return Math.floor(Math.random() * (max - min + 1) + min); 
+  return  Math.floor(Math.random() * (max - min + 1) + min); 
 }
 Locations.prototype.getCustomer = function() { 
-  this.customer = randCustNum(this.min, this.max) + ' cookies'
+  this.customer = randCustNum(this.min, this.max);
 }
+
 const seattle = new Locations('Seattle','./img/SeattleViewinblue.jpeg',23,26,6.3); 
 console.log(seattle);
 console.log(seattle.getCustomer());
@@ -30,28 +32,29 @@ const lima = new Locations('Lima', './img/Lima.jpeg',2,16,4.6);
 console.log(lima); 
 console.log(lima.getCustomer());
 
+ 
 const operatingHours = ['6am', '7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm'];
+
 
 Locations.prototype.generateSales = function() {
   this.grandTotal =0; 
   this.hourlySales = []; 
-    for(let i=0; i<operatingHours.length; i++){
-    let customers = this.getCustomer(); 
-    let soldCookies = Math.floor(customers * this.avgCustomer);
-     this.grandTotal += soldCookies; 
-     console.log(soldCookies);
-    this.hourlySales.push(soldCookies); 
+  for (let i = 0; i < operatingHours.length; i++){
+    let cookiesSold = Math.floor(this.customer * this.avgCustomer);
+    this.grandTotal += cookiesSold; 
+    this.getCustomer(); 
+    this.hourlySales.push(cookiesSold); 
   }
 }
-
-seattle.generateSales();  
+seattle.generateSales(); 
 tokyo.generateSales(); 
 dubai.generateSales(); 
-paris.generateSales(); 
+paris.generateSales();
 lima.generateSales(); 
 
 const locationDivElement = document.getElementById("locations"); 
 const locationTableArrayRows = ['seattle', 'tokyo', 'dubai', 'paris', 'lima']; 
+
 Locations.prototype.render = function() { 
 
   const locationArticle = document.createElement('article'); 
@@ -66,25 +69,51 @@ Locations.prototype.render = function() {
   imgElement.setAttribute('alt', this.name); 
   imgElement.setAttribute('id', 'allImages'); 
   locationArticle.appendChild(imgElement); 
-
-  const tableElement = document.createElement('table');
-  locationArticle.appendChild(tableElement);
-
-  for(let j = 0; j < locationTableArrayRows.length; j++){
-  const trElement = document.createElement('tr'); 
-  trElement.textContent = locationTableArrayRows[j];  
-  tableElement.appendChild(trElement); 
-    for(let i = 0; i < operatingHours.length; i++){
-    const thElementOne = document.createElement('th');
-    thElementOne.textContent = operatingHours[i];
-    trElement.appendChild(thElementOne);
-  }
-
-  }
 }
-
 seattle.render(); 
 tokyo.render(); 
-dubai.render(); 
-paris.render(); 
+dubai.render();
+paris.render();
 lima.render(); 
+
+const locationArticle = document.createElement('article'); 
+locationDivElement.appendChild(locationArticle); 
+
+const tableElement = document.createElement('table');
+locationArticle.appendChild(tableElement);
+
+const thElement = document.createElement('th'); 
+tableElement.appendChild(thElement);
+
+
+
+for(let i = 0; i < operatingHours.length; i++){
+const thElement = document.createElement('th'); 
+thElement.textContent = operatingHours[i];
+tableElement.appendChild(thElement);
+}
+const locationsArray = [seattle, tokyo, dubai, paris, lima]; 
+
+for(let j = 0; j < locationsArray.length; j++){
+const trElement = document.createElement('tr'); 
+trElement.textContent = locationsArray[j].name;  
+tableElement.appendChild(trElement); 
+
+    for(let k = 0; k < operatingHours.length; k++){
+    const tdElement = document.createElement('td');
+    tdElement.textContent = locationsArray[j].hourlySales[k]; 
+    trElement.appendChild(tdElement); 
+  }    
+
+}
+let grandTotals = 0; 
+for(let j = 0; j<locationsArray.length; j++){
+  grandTotals += locationsArray[j].grandTotal; 
+}
+const tfootElement = document.createElement('tfoot'); 
+tableElement.appendChild(tfootElement);
+
+const trFootElement = document.createElement('td'); 
+trFootElement.textContent = grandTotals + ' Total Cookies Sold'; 
+tfootElement.appendChild(trFootElement); 
+
